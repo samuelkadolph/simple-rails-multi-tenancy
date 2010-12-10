d891bfe99d01ada8939e745f654093dc0ee53092
Samuel Kadolph <samuel@kadolph.com>
2010-12-09 23:51:34 -0500

    Remove fluff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 SimpleRailsMultiTenancy::Application.config.secret_token = '085cab704e0b825c598d90e7cf8dcd9fe19f5936c21069860c1b841398ef540630de9300f0b1e06204d50222d05f20e5ef1a456f6352db48f1710dbae08144d6'

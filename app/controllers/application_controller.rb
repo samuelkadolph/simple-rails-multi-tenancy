aade0bfb70d651ac4062fd63d695a557328df490
Samuel Kadolph <samuel@kadolph.com>
2010-12-10 00:29:39 -0500

    Add before_filter to find the Tenant for each request otherwise render friendly error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter do
+    @tenant = Tenant.current = Tenant.find_by_host(request.host)
+    render 'shared/no_such_tenant' unless @tenant
+  end
 end
